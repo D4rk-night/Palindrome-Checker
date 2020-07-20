b3aec5739bd7cf0bd63274ecb0385681afef7f3d
Gabriel Jesus <60709667+D4rk-night@users.noreply.github.com>
2020-07-20 17:38:41 -0300

    Add files via upload

--- Palindrome_checker.js
+++ Palindrome_checker.js
@@ -5,6 +5,7 @@ function palindrome() {
   var regex = /[a-z0-9]/i;
   var outPut = document.getElementById("output");
 
+  //delete spaces and especial letters
   for (var i = 0; i < str.length; i++) {
     if (regex.test(str[i]) == true) {
       word += str[i].toLowerCase();     
@@ -16,6 +17,7 @@ function palindrome() {
     .reverse()
     .join("");
 
+  //verify if is or not a palindrome
   if (word === spelledWord) {
     console.log(true);
     outPut.style.color = "green";
@@ -27,8 +29,14 @@ function palindrome() {
   }
 }
 
+//reset the input
 function reset() {
   document.getElementById("user_input").value = "";
   console.log("deleted");
+}
+
+//info button
+function info() {
+  alert("A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing. You need to write an string and the program will return true if the given string is a palindrome. Otherwise, return false.");
 }
 
